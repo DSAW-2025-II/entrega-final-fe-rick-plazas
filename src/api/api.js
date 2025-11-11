caa0b0d5b5bb0fdadc2b37aaf60b0fcc1d6ffc38
lPlazas <rplazas200@hotmail.com>
2025-11-11 07:55:48 -0500

    Corrige error typo en commit anterior

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "wheels-back-production.up.railway.app",
+  baseURL: "https://wheels-back-production.up.railway.app",
 });
 
 export default API;
